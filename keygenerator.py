34621e601919902bbb60d9a199c43b1b3c988a6c
ArabicMusk <aboudawak@gmail.com>
2025-04-13 14:20:37 -0400

    Create keygenerator.py

---
+++ keygenerator.py
@@ -0,0 +1,17 @@
+from numpy import random
+
+i = 0
+b = "abcdefghijklmnopqrstuvwxyz"
+a = b.upper()
+c = "\\!@#$%^&*()_+-=}{|:><?[];/.,|"
+d = ""
+while i < 24:
+ x = random.randint(9)
+ y = random.randint(25)
+ z = random.randint(25)
+ t = random.randint(25)
+ d = f"{d}{x}{a[z]}{b[y]}{c[t]}"
+ i = i+1
+d = list(d)
+random.shuffle(d)
+print("key ",''.join(d))
